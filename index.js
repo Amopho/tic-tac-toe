9abe7b63303cb703b3be5dff9ef4e31dcd65348f
Amopho <amopho78@gmail.com>
2021-05-12 15:36:55 +0200

    tic-tac-toe

---
+++ index.js
@@ -0,0 +1,17 @@
+// grabbing the HTML elements
+
+// variables for symbols
+
+// variables for game status
+
+// function to check if the symbol is a letter or icon
+
+// function to stop the game and send a message to status
+
+//function that gives distinct variable (topLeft,...) to every element from array off the same cells
+
+//function that checs the winning by comparing if 3 same contents are in a one line
+
+//reset, witha target function within, and another to check whose next, and if the game continous
+
+//event listeners for reset and clicking on cell while playing
